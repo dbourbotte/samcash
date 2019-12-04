06603109f203e918f588ff8047ac7647c9523dc4
Your Name <you@example.com>
2019-12-04 13:40:00 +0100

    ajout de la page d'acceuil

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sam Cash Béthune - ACHAT VENTES PRODUIT NEUFS ET D'OCCASION </title>
+</head>
+<body>
+    
+</body>
+</html>
